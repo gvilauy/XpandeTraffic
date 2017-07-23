8533aaccf9a301d314eb7d055359b0a0fdb039fb
gvilauy <gabrielvila13@gmail.com>
2017-07-23 09:30:07 -0300

    Initial commit

---
+++ src/org/xpande/traffic/model/Init.java
@@ -0,0 +1,8 @@
+package org.xpande.traffic.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 7/23/17.
+ */
+public class Init {
+}
